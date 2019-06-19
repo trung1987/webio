b5f9ae9d61de583f5a0dc211884417867b30e655
TrungT <trungth@sunnypoints.net>
2019-06-19 16:26:55 +0700

    updated

--- src/testcase/G001_LoginFlow.js
+++ src/testcase/G001_LoginFlow.js
@@ -3,6 +3,7 @@ const homeID = require(process.cwd()+'/src/interface/HomeInterface.js');
 const dataTest = require(process.cwd()+'/src/commons/dataTest.js');
 const assert = require('assert');
 const util = require('util');
+const addContext = require('mochawesome/addContext');
 
 let url ='http://demo.guru99.com/v4/';
 describe('Login flow', () => {
@@ -10,16 +11,19 @@ describe('Login flow', () => {
         browser.url(url);
         browser.pause(2000);
     });
+    beforeEach(() => {
+        addContext('aaaa');
+    });
     it('Login ok', () => {  
-        await $(loginID.usernameXpath).setValue(dataTest.UserID);
-        await $(loginID.passXpath).setValue(dataTest.Pass);
-        await browser.$(loginID.btnLoginXpath).click();
+         $(loginID.usernameXpath).setValue(dataTest.UserID);
+         $(loginID.passXpath).setValue(dataTest.Pass);
+         browser.$(loginID.btnLoginXpath).click();
         let temp = util.format(homeID.welcomeMessXpath,dataTest.UserID);
         let elem =  browser.$(temp);
         let isDisplayed = elem.isDisplayed();
         
-        await console.log(isDisplayed);
-        await assert(isDisplayed === true) // true
+         console.log(isDisplayed);
+         assert(isDisplayed === true) // true
     });
 
 
--- test.config.js
+++ test.config.js
@@ -3,7 +3,7 @@ exports.config = {
     specs: [
         'src/testcase/G001_LoginFlow.js'
     ],
-
+   
     runner: 'local',
     host: 'localhost',
     port: 9515,
@@ -15,11 +15,22 @@ exports.config = {
     ],
     reporters: [
        
-        ['junit', {
-            outputDir: './my_report',
-            packageName: process.env.USER_ROLE // chrome.41 - administrator
-        }]
+     //   ['junit', {
+      //      outputDir: './my_report',
+        //    packageName: process.env.USER_ROLE // chrome.41 - administrator
+        //}]
+        ['mochawesome',{
+            outputDir: './Results',
+            outputFileFormat: function(opts) { 
+                return `results-${opts.cid}.${opts.capabilities}.json`
+            }
+          }],
+          
     ],
+    mochawesomeOpts: {
+        includeScreenshots:true,
+        screenshotUseRelativePath:true
+    },
     framework: 'mocha',
     mochaOpts: {
         ui: 'bdd',
